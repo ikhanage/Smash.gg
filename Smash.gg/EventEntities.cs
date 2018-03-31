30dec2cf3b1714a4928fef67d51156cf57ab281c
ikhanage <quiteimran@yahoo.co.uk>
2018-03-31 22:33:31 +0100

    Don't even know how I made this mistake!

--- Smash.gg/EventEntities.cs
+++ Smash.gg/EventEntities.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 
 namespace Smash.gg
@@ -7,7 +6,7 @@ namespace Smash.gg
     public class EventEntities
     {
         [JsonProperty("event")] public Event Event { get; set; }
-        [JsonProperty("groups")] public List<Group> Groups { get; set; }
+        [JsonProperty("groups")] public List<Groups> Groups { get; set; }
         [JsonProperty("phase")] public List<Phase> Phase { get; set; }
     }
 }
