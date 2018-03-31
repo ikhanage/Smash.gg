a0e0d9d30fd1062732df33b7df8ee88f08edc448
ikhanage <quiteimran@yahoo.co.uk>
2018-03-31 21:47:43 +0100

    Adding phase group and list of groups to Entities

---
+++ Smash.gg/PhaseGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Smash.gg
+{
+    public class PhaseGroup
+    {
+        [JsonProperty("entities")] public Entities Entities { get; set; }
+        [JsonProperty("result")] public int Result { get; set; }
+        [JsonProperty("resultEntity")] public string ResultEntity { get; set; }
+        [JsonProperty("actionRecords")] public List<object> ActionRecords { get; set; }
+    }
+}
--- Smash.gg/Properties/AssemblyInfo.cs
+++ Smash.gg/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Smash.gg")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# classes for the Smash.gg api")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Smash.gg")]
